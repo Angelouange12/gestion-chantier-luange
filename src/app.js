79cdd0fe2e3dbb7ce1c36266e168d82e82c0e597
BlazeRUMARIZA <156286605+BlazeRUMARIZA@users.noreply.github.com>
2025-12-29 17:55:46 +0200

    🐛 Fix: Correct routes path in src/app.js (../routes instead of ./routes)

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const logger = require('./middlewares/logger');
-const routes = require('./routes');
+const routes = require('../routes');
 
 const app = express();
 
